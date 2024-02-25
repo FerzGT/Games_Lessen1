f1c3e5b4af8cdb7abc46049d5ba84bad3cae6923
FerzGT <viktorlfes@rambler.ru>
2024-02-25 18:59:27 +0300

    Домашнее задание к лекции 4 «Объекты»

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
+
+
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
   
+      this.marks === undefined ? console.log(`Студент ${this.name} отчислен. Добавление оценок невозможно.`) : this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-  
+ 
+  if (!this.marks || this.marks.length === 0) {
+    return 0;
+  }
+  const sum = this.marks.reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue;
+  });
+  return +(sum / this.marks.length).toFixed(1);
+
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
 }
